8b5ad93cf969089029f59df7dbb3cd1ed92e9370
Anton Patenko <patenko.anton15@gmail.com>
2022-01-19 11:58:20 +1100

    [address_range]: fixed address range generation.
    The mask was applied incorrectly.

--- address_range.cpp
+++ address_range.cpp
@@ -8,6 +8,7 @@
 #define INVALID_ADDRESS     ("")
 #define INVALID_MASK        (-1)
 #define IP_ADDRESS_NUMBERS  (4)
+#define IP_ADDRESS_SIZE_BITS  (32)
 
 // TODO: replace string arg with const string &.
 AddressRange::AddressRange(std::string address_and_mask)
@@ -34,18 +35,20 @@ AddressRange::AddressRange(std::string address_and_mask)
 
 void AddressRange::generate_address_range(std::string address, int mask)
 {
-    // IP address represented as a single number: (192 << 24) | (168 << 16) | (0 << 8) | (1 << 0) = 192.168.0.1
-    uint32_t address_number = 0;
-    this->address_string_to_octets(address, address_number);
+    // IP address represented as octets: (192 << 24) | (168 << 16) | (0 << 8) | (1 << 0) = 192.168.0.1
+    uint32_t address_octets = 0;
+    this->address_string_to_octets(address, address_octets);
     
     // Go through all possible IP addresses and store them into address_range vector.
-    uint32_t null_mask = 0xFFFFFFFF << mask;
-    uint32_t nulled_address = address_number & null_mask;
-    uint32_t mask_bits_max = pow(2, mask) - 1;
-    for (uint32_t mask_bits = 1; mask_bits <= mask_bits_max; mask_bits++) {
+    uint32_t mask_number_of_bits = IP_ADDRESS_SIZE_BITS - mask;
+    uint32_t nulled_address = address_octets & (0xFFFFFFFF << mask_number_of_bits);
+    uint32_t max_mask = pow(2, mask_number_of_bits) - 1;
+    for (uint32_t mask_bits = 0; mask_bits <= max_mask; mask_bits++) {
         std::string current_address;
-        this->address_octets_to_string(current_address, nulled_address | mask_bits);
+        uint32_t current_address_octets = nulled_address | mask_bits;
+        this->address_octets_to_string(current_address, current_address_octets);
         this->address_range.push_back(current_address);
+        std::cout << "generated IP address: " << current_address << std::endl;
     }
 }
 
