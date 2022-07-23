be79a07c8a8930a11b413e4880891b551e9c6f67
Anton Patenko <patenko.anton15@gmail.com>
2022-07-23 21:19:16 +1000

    [ping_subnet]: whitespace.

--- ping_subnet.h
+++ ping_subnet.h
@@ -10,7 +10,6 @@
 class PingSubnet {
     std::shared_ptr<AddressRange> address_range;
     std::shared_ptr<ICMPSocket> icmp_socket;
-    
 
     int send_icmp_request(uint32_t dest_ip);
     int receive_icmp_reply(std::vector<char> &receive_buffer);
